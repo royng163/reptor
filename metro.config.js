490eb10613123a8bb971a3c81dde836c7ce102e3
royng163 <royng163@gmail.com>
2026-04-12 16:44:10 +0800

    feat: build with EAS

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withUniwindConfig } = require('uniwind/metro');
-const path = require('path');
-
-const reptorCorePath = path.resolve(__dirname, '../reptor-core');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push('tflite', 'task');
-
-config.watchFolders = [...(config.watchFolders ?? []), reptorCorePath];
-
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  '@royng163/reptor-core': reptorCorePath,
-};
 
 module.exports = withUniwindConfig(config, {
   // relative path to your global.css file (from previous step)
